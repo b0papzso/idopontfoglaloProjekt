9ebcf6549e9d98f8872ec5830159a087fc2a2f2b
b0papzso <b0papzso@vasvari.org>
2025-03-05 14:03:49 +0100

    Nap választás létrekött

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+  <h1>Időpont foglalás műszaki vizsgára</h1>
+  <label for="napSelect">Nap választása</label>
+  <select name="napSelect" id="napSelect" class="form-select">
+    <option value="Hetfo">Hétfő</option>
+    <option value="Kedd">Kedd</option>
+    <option value="Szerda">Szerda</option>
+    <option value="Csutortok">Csütörtök</option>
+    <option value="Pentek">Péntek</option>
+  </select>
+
+  </div>
 </template>
