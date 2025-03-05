bb8127065194567a841487a31465c91c1caec0b8
b0papzso <b0papzso@vasvari.org>
2025-03-05 14:14:44 +0100

    foglalasok backend létrejött, fetch kész

---
+++ src/stores/idopontok.js
@@ -0,0 +1,11 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useFoglalasStore = defineStore('foglalasok', () => {
+  const foglalasok = ref([])
+  const loadAll = () =>{
+    fetch("http://localhost:3000/foglalasok")
+    .then(resp => resp.json())
+    .then(data => foglalasok.value = data)
+  }
+})
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <script setup>
-
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter()
+var day = ref()
+var showHours = false;
+if(day.value != null)
+{
+  showHours = true
+}
 </script>
 
 <template>
   <div class="container">
   <h1>Időpont foglalás műszaki vizsgára</h1>
   <label for="napSelect">Nap választása</label>
-  <select name="napSelect" id="napSelect" class="form-select">
+  <select name="napSelect" id="napSelect" v-model="day" class="form-select">
     <option value="Hetfo">Hétfő</option>
     <option value="Kedd">Kedd</option>
     <option value="Szerda">Szerda</option>
     <option value="Csutortok">Csütörtök</option>
     <option value="Pentek">Péntek</option>
   </select>
-
+  <div class="table" v-if="showHours">Krumplibogár</div>
   </div>
 </template>
